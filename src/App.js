e420453b84abafae816c7aab1f01cc5628b7d95f
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-06-05 12:32:40 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import mqtt from 'mqtt';
 import './App.css';
 
-function App() {
+let client = null;
+
+const App = () => {
+  const BtnRef = useRef();
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    client = mqtt.connect('ws://mqtt.raghavdhingra.com', {
+      username: 'raghavdhingra',
+      password: 'qwerty1234',
+      protocol: 'ws',
+      port: '3033',
+    });
+    client.subscribe('yo');
+    client.on('connect', (data) => {
+      setIsConnected(true);
+    });
+    client.on('message', (topic, payload, packet) => {
+      console.log('===');
+      console.log(topic);
+      console.log(new TextDecoder().decode(payload));
+      console.log(packet);
+    });
+  }, []);
+
+  useEffect(() => {
+    BtnRef.current.addEventListener('mousedown', () => {
+      client.publish('yo', 'Button press ho gya!');
+    });
+    BtnRef.current.addEventListener('mouseup', () => {
+      client.publish('yo', 'kyu bhai! Button Upr aa gya');
+    });
+  }, [BtnRef]);
+
+  // useEffect(() => {
+  //   document.addEventListener('keydown', (e) => {
+  //     console.log(e);
+  //     // client.publish('yo', 'Key Press ho gyi!');
+  //   });
+  // }, []);
+  // const sendPacket = () => {
+  //   client.publish('yo', 'kyu bhai! pahuch gya packet!');
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="connection-header">Internet Controlled Car</div>
+      <div className="connection-header">
+        <div
+          className={isConnected ? 'header-connected' : 'header-not-connected'}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {isConnected ? 'Connected' : 'Not Connected'}
+        </div>
+      </div>
+      <div
+        ref={BtnRef}
+        //  onClick={sendPacket}
+        className="btn"
+      >
+        Click
+      </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Login from './component/Login/Login';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
