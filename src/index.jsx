665571634b6b65d6ad08e7739cf835d1e0b8e87a
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-06-05 14:08:53 +0530

    folders refactored

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Login from "./script/components/Login/Login";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
