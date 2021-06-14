eea6d5fd86bf04b2402f63a94e8152d70b9217cd
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-06-14 16:19:36 +0530

    touch passivity fix for mobile

--- src/script/common/button/roundButton.jsx
+++ src/script/common/button/roundButton.jsx
@@ -19,12 +19,28 @@ const RoundButton = ({
     success: 'success',
   };
 
+  function absorbEvent_(event) {
+    var e = event || window.event;
+    e.stopPropagation && e.stopPropagation();
+    e.cancelBubble = true;
+    e.returnValue = false;
+    return false;
+  }
+
   return (
     <button
       onMouseDown={onMouseDown}
       onMouseUp={onMouseUp}
-      onTouchStart={onMouseDown}
-      onTouchEnd={onMouseUp}
+      onTouchStart={(e) => {
+        onMouseDown();
+        absorbEvent_(e);
+      }}
+      onTouchMove={absorbEvent_}
+      onTouchCancel={absorbEvent_}
+      onTouchEnd={(e) => {
+        onMouseUp();
+        absorbEvent_(e);
+      }}
       onClick={onClick}
       className={`button button--round button__${margin}Margin button__${
         variant ? variantClass[variant] : variantClass.primary
